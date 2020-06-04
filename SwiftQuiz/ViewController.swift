d96cb13a8fe9a340218ac2e6edbd707bc649311e
Denis Vlaskin <denis@Mac-mini-Denis.lan>
2020-06-04 13:24:15 +0400

    initial

---
+++ SwiftQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Denis Vlaskin on 04.06.2020.
+//  Copyright © 2020 Denis Vlaskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
